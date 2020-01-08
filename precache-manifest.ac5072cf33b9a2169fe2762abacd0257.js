57c16e8b810e9aa7d259878e850d0859cacc8000
dr6am <alexanderbasoc@gmail.com>
2020-01-08 18:21:34 +0300

    Updates

---
+++ precache-manifest.ac5072cf33b9a2169fe2762abacd0257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63041e4bbddd524d4940c0660d7a7d19",
+    "url": "dr6am.github.io/Portfolio-homepage-app/index.html"
+  },
+  {
+    "revision": "8837f58fb7cbf4134d82",
+    "url": "dr6am.github.io/Portfolio-homepage-app/static/css/main.19a8ad70.chunk.css"
+  },
+  {
+    "revision": "af1a2d545d3b3d6a85c4",
+    "url": "dr6am.github.io/Portfolio-homepage-app/static/js/2.940e25f5.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "dr6am.github.io/Portfolio-homepage-app/static/js/2.940e25f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8837f58fb7cbf4134d82",
+    "url": "dr6am.github.io/Portfolio-homepage-app/static/js/main.323ddbf9.chunk.js"
+  },
+  {
+    "revision": "77b05bddfb5779675d0b",
+    "url": "dr6am.github.io/Portfolio-homepage-app/static/js/runtime-main.f74056ed.js"
+  }
+]);
